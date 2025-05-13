aefa8d6d52c5478f6f08c50e0f24e16ec70fc0b4
Marco <marco.baresi01@gmail.com>
2025-05-13 23:22:05 +0200

    setup logger in main of each environment (#12)

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
 import 'package:unizen/config/dependencies.dart';
@@ -9,7 +10,11 @@ import 'package:unizen/main.dart';
 /// Launch with `flutter run --target lib/main_dev.dart`.
 Future main() async {
   await dotenv.load(fileName: '.env.dev');
-  // TODO setup logging based on env variable
+
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
 
   runApp(MultiProvider(providers: providersLocal, child: const UnizenApp()));
 }
