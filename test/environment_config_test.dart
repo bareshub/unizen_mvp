48583fc068f7474918f39f75667aba4dff09be80
Marco <marco.baresi01@gmail.com>
2025-05-17 18:47:01 +0200

    add new environment config tests (#1)

--- test/environment_config_test.dart
+++ test/environment_config_test.dart
@@ -1,12 +1,32 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
+void mainDev() {
   setUpAll(() async {
     await dotenv.load(fileName: ".env.dev");
   });
 
   test('Ensure .env.dev file loaded properly', () {
     expect(dotenv.env['ENVIRONMENT'], 'dev');
+  });
+}
+
+void mainStaging() {
+  setUpAll(() async {
+    await dotenv.load(fileName: ".env.staging");
+  });
+
+  test('Ensure .env.staging file loaded properly', () {
+    expect(dotenv.env['ENVIRONMENT'], 'staging');
+  });
+}
+
+void mainProd() {
+  setUpAll(() async {
+    await dotenv.load(fileName: ".env.prod");
+  });
+
+  test('Ensure .env.prod file loaded properly', () {
+    expect(dotenv.env['ENVIRONMENT'], 'prod');
   });
 }
