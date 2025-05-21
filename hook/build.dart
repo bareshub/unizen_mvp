1052c3d244dd7b88d19e163f08b4ec01c23ecd33
Marco <marco.baresi01@gmail.com>
2025-05-21 23:09:58 +0200

    add zombie sample glb 3d model (#21)

---
+++ hook/build.dart
@@ -0,0 +1,13 @@
+import 'package:native_assets_cli/native_assets_cli.dart';
+import 'package:flutter_scene_importer/build_hooks.dart';
+
+void main(List<String> args) {
+  build(args, (config, output) async {
+    buildModels(
+      buildInput: config,
+      inputFilePaths: [
+        'zombie_after_blender.glb',
+      ],
+    );
+  });
+}
