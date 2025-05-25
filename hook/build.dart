c44ad77639498cde9cb7570c6e99ba18a0b5d1e6
Marco <marco.baresi01@gmail.com>
2025-05-25 23:33:12 +0200

    clear build hook from .glb test models (#23)

--- hook/build.dart
+++ hook/build.dart
@@ -3,12 +3,6 @@ import 'package:flutter_scene_importer/build_hooks.dart';
 
 void main(List<String> args) {
   build(args, (config, output) async {
-    buildModels(
-      buildInput: config,
-      inputFilePaths: [
-        'zombie_after_blender.glb', //
-        'toilet_after_blender.glb', //
-      ],
-    );
+    buildModels(buildInput: config, inputFilePaths: []);
   });
 }
