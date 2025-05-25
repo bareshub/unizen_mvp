faa18737298d98abaee38cb44afed99c4910225c
Marco <marco.baresi01@gmail.com>
2025-05-25 23:26:18 +0200

    scene config file updated to use enum instead of atomic values (#23)

--- lib/ui/animated_scene/models/scene_config.dart
+++ lib/ui/animated_scene/models/scene_config.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
 class SceneConfig {
   final String modelAssetPath;
-  final double turnOffset; // TODO create an enum instead of using a double
-  final List<String> animationNames;
+  final Animation defaultAnimation;
+  final TurnOffset turnOffset;
   final double environmentIntensity;
   final double environmentExposure;
   final double cameraDistance;
@@ -9,11 +11,23 @@ class SceneConfig {
 
   const SceneConfig({
     required this.modelAssetPath,
-    this.turnOffset = 0.5,
+    this.defaultAnimation = Animation.idle,
+    this.turnOffset = TurnOffset.turn45,
     this.environmentIntensity = 1.0,
     this.environmentExposure = 4.0,
-    this.animationNames = const ['idle'],
     this.cameraDistance = 10.0,
     this.fps = 60.0,
   });
+}
+
+enum Animation { idle, walk, attack, death }
+
+enum TurnOffset {
+  turn45(pi / 4),
+  turn90(pi / 2),
+  turn180(pi);
+
+  const TurnOffset(this.radians);
+
+  final num radians;
 }
