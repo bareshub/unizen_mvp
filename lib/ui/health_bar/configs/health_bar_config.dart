a9f2a0fc30b4d11a458264132b10d90da3456516
Marco <marco.baresi01@gmail.com>
2025-06-04 23:12:40 +0200

    update health bar config to use an enum to represent the health bar size (#25)

--- lib/ui/health_bar/configs/health_bar_config.dart
+++ lib/ui/health_bar/configs/health_bar_config.dart
@@ -1,11 +1,15 @@
 class HealthBarConfig {
-  final int maxHealth;
   final HealthBarSize size;
 
-  const HealthBarConfig({
-    required this.maxHealth,
-    this.size = HealthBarSize.medium,
-  });
+  const HealthBarConfig({this.size = HealthBarSize.medium});
 }
 
-enum HealthBarSize { small, medium, large }
+enum HealthBarSize {
+  small(height: 16.0),
+  medium(height: 20.0),
+  large(height: 24.0);
+
+  const HealthBarSize({required this.height});
+
+  final double height;
+}
