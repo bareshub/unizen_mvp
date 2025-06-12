3c408831b1f548b84644aec8cdecc38ec8df6011
Marco <marco.baresi01@gmail.com>
2025-06-12 20:44:21 +0200

    [refactor] create a widget for health_bar_background (#25)

---
+++ lib/ui/health_bar/widgets/health_bar_background.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:unizen/ui/core/ui/frosted_glass_box.dart';
+
+class HealthBarBackground extends StatelessWidget {
+  const HealthBarBackground({
+    super.key,
+    this.margin,
+    required this.height,
+    required this.child,
+  });
+
+  final double height;
+  final EdgeInsetsGeometry? margin;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return FrostedGlassBox(
+      width: double.infinity,
+      height: height,
+      margin: margin,
+      child: Container(child: child),
+    );
+  }
+}
