e84814ecbcf998dcd314f5b5f4b9bda41187d517
Marco <marco.baresi01@gmail.com>
2025-06-15 18:40:24 +0200

    upgrade health_bar_fill with color and width animations (#25)

--- lib/ui/health_bar/widgets/health_bar_fill.dart
+++ lib/ui/health_bar/widgets/health_bar_fill.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:unizen/ui/core/ui/frosted_glass_box.dart';
 import 'package:unizen/ui/health_bar/widgets/health_bar_style.dart';
@@ -5,36 +7,59 @@ import 'package:unizen/ui/health_bar/widgets/health_bar_style.dart';
 class HealthBarFill extends StatelessWidget {
   const HealthBarFill({
     super.key,
-    required this.healthPercentage,
+    required this.health,
+    required this.maxHealth,
     required this.child,
   });
 
-  final double healthPercentage;
+  final int health;
+  final int maxHealth;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      alignment: Alignment.centerLeft,
-      widthFactor: healthPercentage.clamp(0.0, 1.0),
-      child: FrostedGlassBox(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 5.0),
-          decoration: BoxDecoration(
-            color: HealthBarStyle.backgroundColor(
-              healthPercentage,
-            ).withAlpha((0.7 * 255).toInt()),
-            border: Border.all(
-              color: HealthBarStyle.borderColor(
-                healthPercentage,
-              ).withAlpha((0.9 * 255).toInt()),
-              width: 1.5,
+    return FrostedGlassBox(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final healthPercentage = (maxHealth <= 0) ? 0.0 : health / maxHealth;
+
+          final minWidth = calculateMinWidth(
+            healthPercentage,
+            constraints.maxWidth,
+          );
+
+          return UnconstrainedBox(
+            constrainedAxis: Axis.vertical,
+            child: AnimatedContainer(
+              constraints: BoxConstraints(minWidth: minWidth),
+              duration: Durations.extralong4,
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 4.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24.0),
+                color: HealthBarStyle.backgroundColor(
+                  healthPercentage,
+                ).withAlpha((0.7 * 255).toInt()),
+                border: Border.all(
+                  color: HealthBarStyle.borderColor(
+                    healthPercentage,
+                  ).withAlpha((0.9 * 255).toInt()),
+                  width: 1.5,
+                ),
+              ),
+              child: child,
             ),
-            borderRadius: BorderRadius.circular(24.0),
-          ),
-          child: child,
-        ),
+          );
+        },
       ),
     );
   }
+
+  double calculateMinWidth(double healthPercentage, double maxWidth) =>
+      switch (health) {
+        >= 1000 => max(healthPercentage * maxWidth, 50.0),
+        >= 100 => 45.0,
+        >= 10 => 40.0,
+        _ => 35.0,
+      };
 }
