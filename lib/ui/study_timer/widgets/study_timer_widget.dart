bbe489e0923b62b5012e7ae9c84a098fda181f1b
Marco <marco.baresi01@gmail.com>
2025-06-18 22:30:02 +0200

    create study_timer widget placeholder (#25)

---
+++ lib/ui/study_timer/widgets/study_timer_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:unizen/ui/core/ui/frosted_glass_box.dart';
+
+class StudyTimer extends StatelessWidget {
+  const StudyTimer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FrostedGlassBox(
+      child: Column(
+        // Exam title
+        // Exam summary
+        // - Timer +
+        // Start button
+      ),
+    );
+  }
+}
