112b19497a2e8879eb0b8d32142eff196742b305
Marco <marco.baresi01@gmail.com>
2025-06-20 23:39:48 +0200

    wip study timer widget (#24)

--- lib/ui/study_timer/widgets/study_timer_widget.dart
+++ lib/ui/study_timer/widgets/study_timer_widget.dart
@@ -26,37 +26,126 @@ class StudyTimer extends StatelessWidget {
           width: size,
           height: size,
           child: FrostedGlassBox(
-            child: Column(
-              children: [
-                // Exam title
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.fromLTRB(48.0, 24.0, 48.0, 0.0),
-                  child: FittedBox(
-                    fit: BoxFit.fitWidth,
-                    alignment: Alignment.center,
+            child: Center(
+              child: Column(
+                children: [
+                  // Exam title
+                  Padding(
+                    padding: const EdgeInsetsGeometry.only(top: 24.0),
                     child: Text(
                       examName,
-                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        height: 1,
-                        color: Theme.of(context).colorScheme.onSurface,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ),
+
+                  // Exam summary
+                  Text(
+                    'Total studied: 3h 45m',
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+
+                  Spacer(),
+
+                  // - Timer +
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.white38,
+                            shadowColor: Theme.of(
+                              context,
+                            ).colorScheme.shadow.withAlpha(32),
+                            elevation: 2,
+                            padding: EdgeInsets.all(0),
+                            minimumSize: Size.fromRadius(16.0),
+                          ),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.remove,
+                            size: 24.0,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Expanded(
+                          child: FittedBox(
+                            fit: BoxFit.contain,
+                            child: Text(
+                              '25',
+                              style: Theme.of(context).textTheme.headlineLarge,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 4.0),
+                        Text(
+                          'min',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                        SizedBox(width: 8.0),
+                        IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.white38,
+                            shadowColor: Theme.of(
+                              context,
+                            ).colorScheme.shadow.withAlpha(32),
+                            elevation: 2,
+                            padding: EdgeInsets.all(0),
+                            minimumSize: Size.fromRadius(16.0),
+                          ),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.add,
+                            size: 24.0,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Spacer(),
+
+                  // Start button
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 24.0),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor:
+                            Theme.of(context).colorScheme.onPrimary,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondary,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                        ),
+                        shadowColor: Theme.of(
+                          context,
+                        ).colorScheme.shadow.withAlpha(32),
+                        elevation: 2,
+                        minimumSize: Size(double.infinity, 56.0),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.play_arrow_rounded, size: 24.0),
+                          SizedBox(width: 4.0),
+                          Text(
+                            'Start Session',
+                            style: Theme.of(
+                              context,
+                            ).textTheme.labelLarge?.copyWith(
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ),
-                // Exam summary
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                  child: FittedBox(
-                    fit: BoxFit.fitWidth,
-                    alignment: Alignment.center,
-                    child: Text('3h e 25 minuti studiati'),
-                  ),
-                ),
-                // - Timer +
-                // Start button
-              ],
+                ],
+              ),
             ),
           ),
         );
