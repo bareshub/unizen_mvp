ef4390401db1ebc8976d459ad49c0f51adcd9a03
Marco <marco.baresi01@gmail.com>
2025-06-22 08:18:54 +0200

    [refactor] improve study_timer widget and update facade (#24)

--- lib/ui/study_timer/study_timer.dart
+++ lib/ui/study_timer/study_timer.dart
@@ -1,1 +1,3 @@
+export 'configs/study_timer_config.dart';
+export 'view_models/study_timer_view_model.dart';
 export 'widgets/study_timer_widget.dart';
--- lib/ui/study_timer/widgets/study_timer_widget.dart
+++ lib/ui/study_timer/widgets/study_timer_widget.dart
@@ -2,154 +2,73 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:unizen/ui/core/ui/frosted_glass_box.dart';
+import 'package:unizen/ui/study_timer/view_models/study_timer_view_model.dart';
+import 'package:unizen/ui/study_timer/widgets/study_timer_controls.dart';
+import 'package:unizen/ui/study_timer/widgets/study_timer_heading.dart';
+import 'package:unizen/ui/study_timer/widgets/study_timer_start.dart';
+import 'package:unizen/ui/study_timer/widgets/study_timer_summary.dart';
 
 class StudyTimer extends StatelessWidget {
-  const StudyTimer({super.key, required this.examName, this.margin});
+  const StudyTimer({
+    super.key,
+    required this.viewModel,
+    required this.examName,
+    this.minSize = 240.0,
+    this.margin,
+  });
 
+  final StudyTimerViewModel viewModel;
   final String examName;
+  final double minSize;
   final EdgeInsetsGeometry? margin;
-
-  final minSize = 240.0;
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        var size = min(
-          constraints.maxWidth - (margin?.horizontal ?? 0),
-          constraints.maxHeight - (margin?.vertical ?? 0),
-        );
+    return ValueListenableBuilder(
+      valueListenable: viewModel.minutes,
+      builder:
+          (context, minutes, _) => LayoutBuilder(
+            builder: (context, constraints) {
+              var size = min(
+                constraints.maxWidth - (margin?.horizontal ?? 0),
+                constraints.maxHeight - (margin?.vertical ?? 0),
+              );
 
-        size = max(size, minSize);
+              size = max(size, minSize);
 
-        return SizedBox(
-          width: size,
-          height: size,
-          child: FrostedGlassBox(
-            child: Center(
-              child: Column(
-                children: [
-                  // Exam title
-                  Padding(
-                    padding: const EdgeInsetsGeometry.only(top: 24.0),
-                    child: Text(
-                      examName,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                  ),
-
-                  // Exam summary
-                  Text(
-                    'Total studied: 3h 45m',
-                    style: Theme.of(context).textTheme.labelSmall,
-                  ),
-
-                  Spacer(),
-
-                  // - Timer +
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+              return SizedBox(
+                width: size,
+                height: size,
+                child: FrostedGlassBox(
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
                       children: [
-                        IconButton(
-                          style: IconButton.styleFrom(
-                            backgroundColor: Colors.white38,
-                            shadowColor: Theme.of(
-                              context,
-                            ).colorScheme.shadow.withAlpha(32),
-                            elevation: 2,
-                            padding: EdgeInsets.all(0),
-                            minimumSize: Size.fromRadius(16.0),
-                          ),
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.remove,
-                            size: 24.0,
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
+                        StudyTimerHeading(title: examName),
+                        StudyTimerSummary(hours: 3, minutes: 45),
+                        Spacer(),
+                        StudyTimerControls(
+                          minutes: minutes,
+                          onPlusClick:
+                              viewModel.incrementable
+                                  ? () =>
+                                      viewModel.incrementTimerCommand.execute()
+                                  : null,
+                          onMinusClick:
+                              viewModel.decrementable
+                                  ? () =>
+                                      viewModel.decrementTimerCommand.execute()
+                                  : null,
                         ),
-                        SizedBox(width: 8.0),
-                        Expanded(
-                          child: FittedBox(
-                            fit: BoxFit.contain,
-                            child: Text(
-                              '25',
-                              style: Theme.of(context).textTheme.headlineLarge,
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 4.0),
-                        Text(
-                          'min',
-                          style: Theme.of(context).textTheme.headlineSmall,
-                        ),
-                        SizedBox(width: 8.0),
-                        IconButton(
-                          style: IconButton.styleFrom(
-                            backgroundColor: Colors.white38,
-                            shadowColor: Theme.of(
-                              context,
-                            ).colorScheme.shadow.withAlpha(32),
-                            elevation: 2,
-                            padding: EdgeInsets.all(0),
-                            minimumSize: Size.fromRadius(16.0),
-                          ),
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.add,
-                            size: 24.0,
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
+                        Spacer(),
+                        StudyTimerStart(onStart: () {}),
                       ],
                     ),
                   ),
-
-                  Spacer(),
-
-                  // Start button
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 24.0),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        foregroundColor:
-                            Theme.of(context).colorScheme.onPrimary,
-                        backgroundColor:
-                            Theme.of(context).colorScheme.secondary,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                        ),
-                        shadowColor: Theme.of(
-                          context,
-                        ).colorScheme.shadow.withAlpha(32),
-                        elevation: 2,
-                        minimumSize: Size(double.infinity, 56.0),
-                      ),
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.play_arrow_rounded, size: 24.0),
-                          SizedBox(width: 4.0),
-                          Text(
-                            'Start Session',
-                            style: Theme.of(
-                              context,
-                            ).textTheme.labelLarge?.copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           ),
-        );
-      },
     );
   }
 }
