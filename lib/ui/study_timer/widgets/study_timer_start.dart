a06d9ea9e7d429ae83319923c82cdb7052d7e570
Marco <marco.baresi01@gmail.com>
2025-06-23 21:44:43 +0200

    [refactor] create frosted_glass_text_button factorization and update study_timer accordingly (#24)

--- lib/ui/study_timer/widgets/study_timer_start.dart
+++ lib/ui/study_timer/widgets/study_timer_start.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unizen/ui/core/ui/frosted_glass_text_button.dart';
 
 class StudyTimerStart extends StatelessWidget {
   const StudyTimerStart({super.key, required this.onStart});
@@ -7,32 +8,10 @@ class StudyTimerStart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      style: TextButton.styleFrom(
-        foregroundColor: Theme.of(context).colorScheme.onPrimary,
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-        ),
-        shadowColor: Theme.of(context).colorScheme.shadow.withAlpha(32),
-        elevation: 2,
-        minimumSize: Size(double.infinity, 56.0),
-        overlayColor: Theme.of(context).colorScheme.onPrimary,
-      ),
-      onPressed: onStart,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.play_arrow_rounded, size: 24.0),
-          SizedBox(width: 4.0),
-          Text(
-            'Start',
-            style: Theme.of(context).textTheme.labelLarge?.copyWith(
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
-          ),
-        ],
-      ),
+    return FrostedGlassTextButton(
+      text: 'Start',
+      action: onStart,
+      icon: Icons.play_arrow_rounded,
     );
   }
 }
