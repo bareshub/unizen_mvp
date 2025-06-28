3d0731598b9e65f706f450a02780a7ed4d3322a4
Marco <marco.baresi01@gmail.com>
2025-06-28 10:24:09 +0200

    create exam model (#26)

---
+++ lib/ui/home_page/models/exam.dart
@@ -0,0 +1,13 @@
+class Exam {
+  const Exam({
+    required this.name,
+    required this.modelAssetPath,
+    required this.maxHealth,
+    required this.health,
+  });
+
+  final String name;
+  final String modelAssetPath;
+  final int maxHealth;
+  final int health;
+}
