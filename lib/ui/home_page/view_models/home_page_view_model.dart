d6417c8fb27d1eb0a3d8fea7e25f031e8cb03f15
Marco <marco.baresi01@gmail.com>
2025-06-28 10:19:21 +0200

    add home_page view model (#26)

---
+++ lib/ui/home_page/view_models/home_page_view_model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_command/flutter_command.dart';
+import 'package:unizen/ui/home_page/models/exam.dart';
+
+class HomePageViewModel extends ChangeNotifier {
+  HomePageViewModel({required this.exams}) {
+    addExamCommand = Command.createSyncNoParamNoResult(() {});
+    deleteExamCommand = Command.createSyncNoParamNoResult(() {});
+  }
+
+  late final Command<void, void> addExamCommand;
+  late final Command<void, void> deleteExamCommand;
+
+  final List<Exam> exams; // TODO replace behaviour through repository
+
+  @override
+  void dispose() {
+    addExamCommand.dispose();
+    deleteExamCommand.dispose();
+    super.dispose();
+  }
+}
