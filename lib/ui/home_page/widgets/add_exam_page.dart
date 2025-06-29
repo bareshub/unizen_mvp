54a53d7f75372324291572c259565547f5d4e770
Marco <marco.baresi01@gmail.com>
2025-06-29 20:05:01 +0200

    create add_exam_page placeholder (#26)

---
+++ lib/ui/home_page/widgets/add_exam_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddExamPage extends StatelessWidget {
+  const AddExamPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
