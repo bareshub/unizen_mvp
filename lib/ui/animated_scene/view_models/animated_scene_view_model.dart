e7dfabf4b7c4dda58b78a2a464a9f5b6b8e6ad38
Marco <marco.baresi01@gmail.com>
2025-06-30 18:55:51 +0200

    extract static resource initialization from animated_scene_view_model (#26)

--- lib/ui/animated_scene/view_models/animated_scene_view_model.dart
+++ lib/ui/animated_scene/view_models/animated_scene_view_model.dart
@@ -5,12 +5,25 @@ import 'package:flutter_scene/scene.dart';
 import '../configs/scene_config.dart';
 
 class AnimatedSceneViewModel extends ChangeNotifier {
+  AnimatedSceneViewModel({required this.config}) {
+    loadCommand = Command.createAsyncNoParamNoResult(_loadScene);
+
+    turnRightCommand = Command.createSyncNoParamNoResult(_turnRight);
+    turnLeftCommand = Command.createSyncNoParamNoResult(_turnLeft);
+    turnOffsetCommand = Command.createSyncNoResult(_turnOffsetCommand);
+
+    playClipCommand = Command.createSyncNoResult<String>(
+      (String clipName) => _play(clipName),
+    );
+  }
+
   final Scene scene = Scene();
   final SceneConfig config;
 
   late final Command<void, void> loadCommand;
   late final Command<void, void> turnRightCommand;
   late final Command<void, void> turnLeftCommand;
+  late final Command<double, void> turnOffsetCommand;
   late final Command<void, void> playClipCommand;
 
   final ValueNotifier<double> elapsedFrames = ValueNotifier(0);
@@ -18,18 +31,7 @@ class AnimatedSceneViewModel extends ChangeNotifier {
 
   double _destinationX = 0;
 
-  Map<String, AnimationClip> animationMap = {};
-
-  AnimatedSceneViewModel({required this.config}) {
-    loadCommand = Command.createAsyncNoParamNoResult(_loadScene);
-
-    turnRightCommand = Command.createSyncNoParamNoResult(_turnRight);
-    turnLeftCommand = Command.createSyncNoParamNoResult(_turnLeft);
-
-    playClipCommand = Command.createSyncNoResult<String>(
-      (String clipName) => _play(clipName),
-    );
-  }
+  final Map<String, AnimationClip> _animationMap = {};
 
   void update(Duration elapsed) {
     elapsedFrames.value = elapsed.inMilliseconds / 1000 * config.fps;
@@ -53,10 +55,10 @@ class AnimatedSceneViewModel extends ChangeNotifier {
     final node = await Node.fromAsset(config.modelAssetPath);
 
     for (final animation in node.parsedAnimations) {
-      animationMap[animation.name] = _createClip(node, animation.name);
+      _animationMap[animation.name] = _createClip(node, animation.name);
     }
 
-    var defaultClip = animationMap[config.defaultAnimation.name];
+    var defaultClip = _animationMap[config.defaultAnimation.name];
     defaultClip
       ?..weight = 1
       ..play();
@@ -65,8 +67,6 @@ class AnimatedSceneViewModel extends ChangeNotifier {
     scene.environment
       ..exposure = config.environmentExposure
       ..intensity = config.environmentIntensity;
-
-    await Scene.initializeStaticResources();
   }
 
   AnimationClip _createClip(Node node, String name) {
@@ -81,8 +81,14 @@ class AnimatedSceneViewModel extends ChangeNotifier {
 
   void _turnLeft() => _destinationX = -config.turnOffset.radians.toDouble();
 
+  // TODO: WIP
+  void _turnOffsetCommand(double offset) {
+    final destinationX = config.turnOffset.radians;
+    rotationX.value = offset.clamp(0.0, 1.0) * destinationX;
+  }
+
   void _play(String clipName) {
-    animationMap.forEach((key, clip) {
+    _animationMap.forEach((key, clip) {
       if (key == clipName) {
         clip
           ..weight = 1
