e96d6cdb7ee08def69459e6ebd795027f0c81b97
Marco <marco.baresi01@gmail.com>
2025-07-01 23:03:09 +0200

    add environmentIntensity and distance properties to exam model (#26)

--- lib/ui/home_page/models/exam.dart
+++ lib/ui/home_page/models/exam.dart
@@ -4,10 +4,14 @@ class Exam {
     required this.modelAssetPath,
     required this.maxHealth,
     required this.health,
+    this.environmentIntensity = 3.0,
+    this.cameraDistance = 10.0,
   });
 
   final String name;
   final String modelAssetPath;
   final int maxHealth;
   final int health;
+  final double environmentIntensity;
+  final double cameraDistance;
 }
