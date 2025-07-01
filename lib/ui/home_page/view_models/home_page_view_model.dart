2a7e05449185f468df525a99418dcfc8cf6ddc4e
Marco <marco.baresi01@gmail.com>
2025-07-01 23:04:34 +0200

    rearrange home_page_view_model (#26)

--- lib/ui/home_page/view_models/home_page_view_model.dart
+++ lib/ui/home_page/view_models/home_page_view_model.dart
@@ -3,15 +3,15 @@ import 'package:flutter_command/flutter_command.dart';
 import 'package:unizen/ui/home_page/models/exam.dart';
 
 class HomePageViewModel extends ChangeNotifier {
-  HomePageViewModel({required this.exams}) {
-    addExamCommand = Command.createSyncNoParamNoResult(() {});
-    deleteExamCommand = Command.createSyncNoParamNoResult(() {});
-  }
-
   late final Command<void, void> addExamCommand;
   late final Command<void, void> deleteExamCommand;
 
   final List<Exam> exams; // TODO replace behaviour through repository
+
+  HomePageViewModel({required this.exams}) {
+    addExamCommand = Command.createSyncNoParamNoResult(() {});
+    deleteExamCommand = Command.createSyncNoParamNoResult(() {});
+  }
 
   @override
   void dispose() {
