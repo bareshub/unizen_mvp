b16348a87c9a7df998729f3b9b136df5c41f0277
Marco <marco.baresi01@gmail.com>
2025-07-03 22:50:38 +0200

    replace modelAssetPath, environmentIntensity and cameraDistance with cceneConfig property in exam model (#26)

--- lib/ui/home_page/models/exam.dart
+++ lib/ui/home_page/models/exam.dart
@@ -1,17 +1,16 @@
+import 'package:unizen/ui/animated_scene/animated_scene.dart';
+
 class Exam {
   const Exam({
     required this.name,
-    required this.modelAssetPath,
     required this.maxHealth,
     required this.health,
-    this.environmentIntensity = 3.0,
-    this.cameraDistance = 10.0,
+    required this.sceneConfig,
   });
 
   final String name;
-  final String modelAssetPath;
   final int maxHealth;
   final int health;
-  final double environmentIntensity;
-  final double cameraDistance;
+
+  final SceneConfig sceneConfig;
 }
