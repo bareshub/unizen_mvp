6f3ffa5f3cd02de978155509bbf89f56115cfd37
Marco <marco.baresi01@gmail.com>
2025-07-07 21:46:40 +0200

    restore exam insted of exam node list model (#26)

--- lib/ui/home_page/view_models/home_page_view_model.dart
+++ lib/ui/home_page/view_models/home_page_view_model.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_command/flutter_command.dart';
 
-import '../models/exam_node.dart';
+import '../models/exam.dart';
 
 class HomePageViewModel extends ChangeNotifier {
   late final Command<void, void> addExamCommand;
   late final Command<void, void> deleteExamCommand;
 
-  final List<ExamNode> examNodes; // TODO replace behaviour through repository
+  final List<Exam> exams; // TODO replace behaviour through repository
 
-  HomePageViewModel({required this.examNodes}) {
+  HomePageViewModel({required this.exams}) {
     addExamCommand = Command.createSyncNoParamNoResult(() {});
     deleteExamCommand = Command.createSyncNoParamNoResult(() {});
   }
