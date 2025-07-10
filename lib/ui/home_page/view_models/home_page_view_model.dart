437a02f345784cd1623c7eff0da383e10db2c400
Marco <marco.baresi01@gmail.com>
2025-07-10 20:05:23 +0200

    [refactor] extract logic from home_page widget into home_page view model (#26)

--- lib/ui/home_page/view_models/home_page_view_model.dart
+++ lib/ui/home_page/view_models/home_page_view_model.dart
@@ -1,23 +1,73 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter_command/flutter_command.dart';
+import 'package:unizen/ui/animated_scene/animated_scene.dart';
+import 'package:unizen/ui/animated_scene/view_models/rotation_view_model.dart';
 
 import '../models/exam.dart';
+import '../widgets/add_exam_page.dart';
+import '../widgets/exam_page.dart';
 
 class HomePageViewModel extends ChangeNotifier {
-  late final Command<void, void> addExamCommand;
-  late final Command<void, void> deleteExamCommand;
+  final List<Exam> exams;
+  late List<RotationViewModel> rotationViewModels;
+  bool sceneReady = false;
 
-  final List<Exam> exams; // TODO replace behaviour through repository
+  HomePageViewModel({required this.exams});
 
-  HomePageViewModel({required this.exams}) {
-    addExamCommand = Command.createSyncNoParamNoResult(() {});
-    deleteExamCommand = Command.createSyncNoParamNoResult(() {});
+  void init(PageController pageController) {
+    AnimatedScene.initialize().then((_) {
+      rotationViewModels = exams.map((_) => RotationViewModel()).toList();
+      sceneReady = true;
+      notifyListeners();
+    });
+
+    pageController.addListener(() {
+      final page = pageController.page ?? 0.0;
+      final intPart = page.floor();
+      final decimalPart = page - intPart;
+
+      if (decimalPart != 0 && exams.elementAtOrNull(intPart) != null) {
+        var lIndex = intPart - 1;
+        var rIndex = intPart;
+        rotationViewModels
+            .elementAt(lIndex)
+            .rotateCommand
+            .execute(decimalPart * pi / 3);
+        rotationViewModels
+            .elementAt(rIndex)
+            .rotateCommand
+            .execute(-(1 - decimalPart) * pi / 3);
+      }
+    });
+  }
+
+  int get pageCount => exams.length + 1;
+
+  List<Widget> buildPages() {
+    return [
+      AddExamPage(),
+      ...exams.asMap().entries.map((entry) {
+        final index = entry.key;
+        final exam = entry.value;
+        final lVerticalText =
+            index == 0 ? 'NEW EXAM' : exams.elementAt(index - 1).name;
+        final rVerticalText =
+            index == exams.length - 1 ? '' : exams.elementAt(index + 1).name;
+        return ExamPage(
+          rotationViewModel: rotationViewModels[index],
+          exam: exam,
+          lVerticalText: lVerticalText,
+          rVerticalText: rVerticalText,
+        );
+      }),
+    ];
   }
 
   @override
   void dispose() {
-    addExamCommand.dispose();
-    deleteExamCommand.dispose();
+    for (var vm in rotationViewModels) {
+      vm.dispose();
+    }
     super.dispose();
   }
 }
