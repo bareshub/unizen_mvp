437f8beea448a88c53361cdc2193686994b4bf8f
Marco <marco.baresi01@gmail.com>
2025-07-10 20:06:15 +0200

    [refactor] update home_page widget according to recent updates (#26)

--- lib/ui/home_page/widgets/home_page_widget.dart
+++ lib/ui/home_page/widgets/home_page_widget.dart
@@ -1,15 +1,8 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-import 'package:unizen/ui/animated_scene/animated_scene.dart';
-import 'package:unizen/ui/animated_scene/view_models/rotation_view_model.dart';
 import 'package:unizen/ui/core/ui/unizen_logo.dart';
-
 import '../view_models/home_page_view_model.dart';
-import '../widgets/add_exam_page.dart';
-import '../widgets/exam_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.viewModel});
@@ -21,48 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<RotationViewModel> rotationViewModels = [];
-
   late final PageController _pageController;
-  bool _sceneReady = false;
 
   @override
   void initState() {
     super.initState();
-
-    AnimatedScene.initialize().then((_) {
-      rotationViewModels =
-          widget.viewModel.exams.map((_) => RotationViewModel()).toList();
-      setState(() {
-        _sceneReady = true;
-      });
-    });
-
     _pageController = PageController(initialPage: 1);
-
-    _pageController.addListener(() {
-      final page = _pageController.page ?? 0.0;
-
-      final intPart = page.floor();
-      final decimalPart = page - intPart;
-
-      if (decimalPart != 0 &&
-          widget.viewModel.exams.elementAtOrNull(intPart) != null) {
-        // setState(() {});
-        var lIndex = intPart - 1;
-        var rIndex = intPart;
-
-        rotationViewModels
-            .elementAt(lIndex)
-            .rotateCommand
-            .execute(decimalPart * pi / 3);
-
-        rotationViewModels
-            .elementAt(rIndex)
-            .rotateCommand
-            .execute(-(1 - decimalPart) * pi / 3);
-      }
-    });
+    widget.viewModel.init(_pageController);
   }
 
   @override
@@ -77,66 +35,27 @@ class _HomePageState extends State<HomePage> {
               children: [
                 SizedBox(width: double.infinity, child: const UnizenLogo()),
                 Expanded(
-                  child:
-                      _sceneReady
-                          ? PageView(
-                            allowImplicitScrolling: true,
-                            controller: _pageController,
-                            children: [
-                              AddExamPage(),
-                              ...widget.viewModel.exams.asMap().entries.map((
-                                entry,
-                              ) {
-                                final index = entry.key;
-                                final exam = entry.value;
-
-                                final lVerticalText = switch (index) {
-                                  0 => 'NEW EXAM',
-                                  _ =>
-                                    widget.viewModel.exams
-                                        .elementAt(index - 1)
-                                        .name,
-                                };
-
-                                final rVerticalText =
-                                    index == widget.viewModel.exams.length - 1
-                                        ? ''
-                                        : widget.viewModel.exams
-                                            .elementAt(index + 1)
-                                            .name;
-
-                                // if (animatedSceneViewModels.length > index) {
-                                //   debugPrint('updated $index');
-                                //   animatedSceneViewModels[index] =
-                                //       AnimatedSceneViewModel(
-                                //         config: examNode.exam.sceneConfig,
-                                //       );
-                                // } else {
-                                //   animatedSceneViewModels.add(
-                                //     AnimatedSceneViewModel(
-                                //       config: examNode.exam.sceneConfig,
-                                //     ),
-                                //   );
-                                // }
-
-                                return ExamPage(
-                                  rotationViewModel: rotationViewModels[index],
-                                  exam: exam,
-                                  lVerticalText: lVerticalText,
-                                  rVerticalText: rVerticalText,
-                                );
-                              }),
-                            ],
-                          )
-                          : Center(
-                            child: CircularProgressIndicator(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
+                  child: AnimatedBuilder(
+                    animation: widget.viewModel,
+                    builder: (context, _) {
+                      if (!widget.viewModel.sceneReady) {
+                        return Center(
+                          child: CircularProgressIndicator(
+                            color: Theme.of(context).colorScheme.onPrimary,
                           ),
+                        );
+                      }
+                      return PageView(
+                        allowImplicitScrolling: true,
+                        controller: _pageController,
+                        children: widget.viewModel.buildPages(),
+                      );
+                    },
+                  ),
                 ),
                 SmoothPageIndicator(
                   controller: _pageController,
-                  count: widget.viewModel.exams.length + 1,
+                  count: widget.viewModel.pageCount,
                   effect: ScrollingDotsEffect(
                     activeDotColor: Theme.of(context).colorScheme.secondary,
                     dotColor: Theme.of(context).colorScheme.primaryContainer,
@@ -154,9 +73,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-    for (var vm in rotationViewModels) {
-      vm.dispose();
-    }
+    widget.viewModel.dispose();
     _pageController.dispose();
     super.dispose();
   }
