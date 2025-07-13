39a07f81c4c7ba11425cdb27fde42aeba3caf548
Marco <marco.baresi01@gmail.com>
2025-07-13 22:35:56 +0200

    extract TurnOffset from scene_config (#26)

--- lib/ui/animated_scene/configs/scene_config.dart
+++ lib/ui/animated_scene/configs/scene_config.dart
@@ -1,9 +1,6 @@
-import 'dart:math';
-
 class SceneConfig {
   final String modelAssetPath;
   final Animation defaultAnimation;
-  final TurnOffset turnOffset;
   final double environmentIntensity;
   final double environmentExposure;
   final double cameraDistance;
@@ -12,7 +9,6 @@ class SceneConfig {
   const SceneConfig({
     required this.modelAssetPath,
     this.defaultAnimation = Animation.idle,
-    this.turnOffset = TurnOffset.turn30,
     this.environmentIntensity = 3.0,
     this.environmentExposure = 4.0,
     this.cameraDistance = 10.0,
@@ -21,13 +17,3 @@ class SceneConfig {
 }
 
 enum Animation { idle, walk, attack, death }
-
-enum TurnOffset {
-  turn15(pi / 24),
-  turn30(pi / 12),
-  turn45(pi / 8);
-
-  const TurnOffset(this.radians);
-
-  final num radians;
-}
