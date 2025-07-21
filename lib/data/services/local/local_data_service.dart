eb552682b6bccb45ecfe776d9b58bab61c067771
Marco <marco.baresi01@gmail.com>
2025-07-21 21:11:25 +0200

    implement the local data service (#39)

--- lib/data/services/local/local_data_service.dart
+++ lib/data/services/local/local_data_service.dart
@@ -1,1 +1,45 @@
-class LocalDataService {}
+import '../../../domain/models/animated_scene/animated_scene.dart';
+import '../../../domain/models/exam/exam.dart';
+
+class LocalDataService {
+  List<Exam> getExams() {
+    return [
+      Exam(
+        id: 1,
+        name: 'AUTOMATION',
+        maxHealth: 5000,
+        health: 2780,
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/zombie_after_blender.model',
+        ),
+      ),
+      Exam(
+        id: 2,
+        name: 'PHYSICS',
+        maxHealth: 5000,
+        health: 4878,
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/toilet_after_blender.model',
+        ),
+      ),
+      Exam(
+        id: 3,
+        name: 'COMPUTER SCIENCE',
+        maxHealth: 5000,
+        health: 1280,
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/zombie_after_blender.model',
+        ),
+      ),
+      Exam(
+        id: 4,
+        name: 'AUTOMATION',
+        maxHealth: 5000,
+        health: 2780,
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/zombie_after_blender.model',
+        ),
+      ),
+    ];
+  }
+}
