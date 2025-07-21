4660c2679f7fe92566b94c92c8c3ab841fed98a2
Marco <marco.baresi01@gmail.com>
2025-07-21 20:58:37 +0200

    provide crud methods to exam repository interface (#39)

--- lib/data/repositories/exam/exam_repository.dart
+++ lib/data/repositories/exam/exam_repository.dart
@@ -1,2 +1,22 @@
+import '../../../domain/models/exam/exam.dart';
+import '../../../utils/result.dart';
+
 /// Data source for exams
-class ExamRepository {}
+abstract class ExamRepository {
+  /// Creates a new [Exam]
+  Future<Result<int>> createExam(Exam exam);
+
+  /// Returns the list of [Exam] for the current user
+  Future<Result<List<Exam>>> getExamsList();
+
+  /// Update an existing [Exam]
+  Future<Result<Exam>> update(
+    int id, {
+    String? name,
+    int? maxHealth,
+    int? health,
+  });
+
+  /// Delete an [Exam]
+  Future<Result<void>> delete(int id);
+}
