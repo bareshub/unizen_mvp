e737070310d3cd698f770fb876eda1e3bf345c50
Marco <marco.baresi01@gmail.com>
2025-07-25 20:31:38 +0200

    create static_scene model and make animated_scene to extend static_scene (#47)

---
+++ lib/domain/models/static_scene/static_scene.dart
@@ -0,0 +1,13 @@
+class StaticScene {
+  final String modelAssetPath;
+  final double environmentIntensity;
+  final double environmentExposure;
+  final double cameraDistance;
+
+  const StaticScene({
+    required this.modelAssetPath,
+    this.environmentIntensity = 3.0,
+    this.environmentExposure = 4.0,
+    this.cameraDistance = 10.0,
+  });
+}
