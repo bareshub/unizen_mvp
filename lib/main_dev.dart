ed0114ab237abe46cdd56505b0142b3a85deb4b4
Marco <marco.baresi01@gmail.com>
2025-07-28 22:56:06 +0200

    configure a global exception handler for dev environment (#47)

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_command/flutter_command.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
@@ -18,6 +19,10 @@ Future main() async {
       print('${record.level.name}: ${record.time}: ${record.message}');
     }
   });
+
+  Command.globalExceptionHandler = (exception, stackTrace) {
+    debugPrint('Global Command Exception: $exception');
+  };
 
   runApp(MultiProvider(providers: providersLocal, child: const UnizenApp()));
 }
