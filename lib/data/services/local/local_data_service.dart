0d5f40480c22b229135dfc74ec2e3208f3b50809
Marco <marco.baresi01@gmail.com>
2025-07-30 23:54:46 +0200

    implement getBosses method in local_data service (#52)

--- lib/data/services/local/local_data_service.dart
+++ lib/data/services/local/local_data_service.dart
@@ -1,44 +1,126 @@
 import '../../../domain/models/animated_scene/animated_scene.dart';
+import '../../../domain/models/boss/boss.dart';
 import '../../../domain/models/exam/exam.dart';
 
 class LocalDataService {
   List<Exam> getExams() {
     return [
       Exam(
-        id: 1,
+        name: 'MACHINE LEARNING',
+        maxHealth: 5000,
+        health: 2780,
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tvwoman.model',
+          cameraDistance: 28,
+        ),
+      ),
+      Exam(
         name: 'AUTOMATION',
         maxHealth: 5000,
         health: 2780,
         animatedScene: AnimatedScene(
-          modelAssetPath: 'build/models/zombie_after_blender.model',
+          modelAssetPath: 'build/models/cameraman_supreme_god.model',
+          cameraDistance: 36,
         ),
       ),
       Exam(
-        id: 2,
         name: 'PHYSICS',
         maxHealth: 5000,
         health: 4878,
         animatedScene: AnimatedScene(
-          modelAssetPath: 'build/models/toilet_after_blender.model',
+          modelAssetPath: 'build/models/skibidi_yisus.model',
+          cameraDistance: 28,
         ),
       ),
       Exam(
-        id: 3,
         name: 'COMPUTER SCIENCE',
         maxHealth: 5000,
         health: 1280,
         animatedScene: AnimatedScene(
-          modelAssetPath: 'build/models/zombie_after_blender.model',
+          modelAssetPath: 'build/models/tv_man_supreme.model',
+          cameraDistance: 16,
         ),
       ),
       Exam(
-        id: 4,
+        name: 'ARTIFICIAL INTELLIGENCE',
+        maxHealth: 5000,
+        health: 2780,
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tvman_multiple_supreme.model',
+          cameraDistance: 20,
+        ),
+      ),
+      Exam(
         name: 'AUTOMATION',
         maxHealth: 5000,
         health: 2780,
         animatedScene: AnimatedScene(
-          modelAssetPath: 'build/models/zombie_after_blender.model',
+          modelAssetPath: 'build/models/tvman_multiple.model',
+          cameraDistance: 32,
         ),
+      ),
+      Exam(
+        name: 'AI ENTREPRENEURSHIP',
+        maxHealth: 5000,
+        health: 2780,
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tvman_supreme.model',
+          cameraDistance: 44,
+        ),
+      ),
+    ];
+  }
+
+  List<Boss> getBosses() {
+    return [
+      Boss(
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tvwoman.model',
+          cameraDistance: 28,
+        ),
+        ects: 3,
+      ),
+      Boss(
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/cameraman_supreme_god.model',
+          cameraDistance: 36,
+        ),
+        ects: 4,
+      ),
+      // Boss(
+      //   animatedScene: AnimatedScene(
+      //     modelAssetPath: 'build/models/skibidi_yisus.model',
+      //     cameraDistance: 28,
+      //   ),
+      //   ects: 5,
+      // ),
+      Boss(
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tv_man_supreme.model',
+          cameraDistance: 16,
+        ),
+        ects: 6,
+      ),
+      Boss(
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tvman_multiple_supreme.model',
+          cameraDistance: 20,
+        ),
+        ects: 7,
+      ),
+      Boss(
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tvman_multiple.model',
+          cameraDistance: 32,
+        ),
+        ects: 8,
+      ),
+      Boss(
+        animatedScene: AnimatedScene(
+          modelAssetPath: 'build/models/tvman_supreme.model',
+          cameraDistance: 44,
+        ),
+        ects: 9,
       ),
     ];
   }
