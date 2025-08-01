228166d245a5037394b7468238cf4ea95c2edaf1
Marco <marco.baresi01@gmail.com>
2025-08-01 15:08:43 +0200

    import new 3d models and update build file accordingly (#47)

--- hook/build.dart
+++ hook/build.dart
@@ -3,6 +3,20 @@ import 'package:flutter_scene_importer/build_hooks.dart';
 
 void main(List<String> args) {
   build(args, (config, output) async {
-    buildModels(buildInput: config, inputFilePaths: []);
+    buildModels(
+      buildInput: config,
+      inputFilePaths: [
+        // 'zombie_after_blender.glb', //
+        // 'toilet_after_blender.glb', //
+        // 'tvman_supreme.glb',
+        // 'cameraman_supreme_god.glb',
+        // 'skibidi_yisus.glb',
+        // 'tv_man_supreme.glb',
+        // 'tvman_multiple_supreme.glb',
+        // 'tvman_multiple.glb',
+        // 'tvman_supreme.glb',
+        // 'tvwoman.glb',
+      ],
+    );
   });
 }
