7d819b1ad6a9e1244ecdb6213668d50e342e936a
Marco <marco.baresi01@gmail.com>
2025-08-04 15:51:13 +0200

    massive update of home_screen view model (#47)

--- lib/ui/home_screen/view_models/home_screen_view_model.dart
+++ lib/ui/home_screen/view_models/home_screen_view_model.dart
@@ -2,130 +2,129 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_command/flutter_command.dart';
-import 'package:unizen/ui/home_screen/widgets/add_exam_page.dart';
 
+import '../../../data/repositories/boss/boss_repository.dart';
 import '../../../data/repositories/exam/exam_repository.dart';
-import '../../animated_scene/view_models/rotation_view_model.dart';
+import '../../../domain/models/boss/boss.dart';
+import '../../../domain/models/exam/exam.dart';
+import '../../../domain/models/exam/exam_page.dart';
 import '../../../utils/result.dart';
 
-import '../../../domain/models/exam/exam.dart';
-import '../widgets/exam_page.dart';
+enum HomeScreenState { initial, loading, loaded, error }
 
 class HomeScreenViewModel extends ChangeNotifier {
-  final ExamRepository _examRepository;
-  final TurnOffset turnOffset;
-
-  late final ValueNotifier<bool> sceneReady;
-  late final ValueNotifier<int> pageCount;
-  late final Command<void, void> loadCommand;
-  late final Command<PageController, void> initCommand;
-
-  List<Exam> _exams = [];
-  List<RotationViewModel> rotationViewModels = [];
-  List<Widget> _pages = [];
-  List<Widget> get pages => _pages;
-
   HomeScreenViewModel({
+    required BossRepository bossRepository,
     required ExamRepository examRepository,
     this.turnOffset = TurnOffset.turn60,
   }) : _examRepository = examRepository {
     sceneReady = ValueNotifier(false);
-    pageCount = ValueNotifier(0);
-    loadCommand = Command.createAsyncNoParamNoResult(_load);
+    screenState = ValueNotifier(HomeScreenState.initial);
+
+    exams = ValueNotifier<List<Exam>>([]);
+    bosses = ValueNotifier<List<Boss>>([]);
+    pages = ValueNotifier<List<ExamPage>>([]);
+
     initCommand = Command.createSyncNoResult<PageController>(_init);
+    loadCommand = Command.createAsyncNoParamNoResult(_load);
+    addExamCommand = Command.createAsyncNoResult<Exam>(_addExam);
+  }
+
+  final ExamRepository _examRepository;
+  final TurnOffset turnOffset;
+
+  late final ValueNotifier<bool> sceneReady;
+  late final ValueNotifier<List<Exam>> exams;
+  late final ValueNotifier<List<Boss>> bosses;
+  late final ValueNotifier<List<ExamPage>> pages;
+  late final ValueNotifier<HomeScreenState> screenState;
+  late final PageController _pageController;
+
+  late final Command<PageController, void> initCommand;
+  late final Command<void, void> loadCommand;
+  late final Command<Exam, void> addExamCommand;
+
+  void _init(PageController pageController) {
+    _pageController = pageController;
   }
 
   Future<void> _load() async {
-    try {
-      final result = await _examRepository.getExamsList();
+    screenState.value = HomeScreenState.loading;
+    final result = await _examRepository.getExamsList();
+    switch (result) {
+      case Ok<List<Exam>>():
+        exams.value = List<Exam>.from(result.value);
+        screenState.value = HomeScreenState.loaded;
+        break;
+      case Error<List<Exam>>():
+        screenState.value = HomeScreenState.error;
+        exams.value = [];
+        break;
+    }
+    _refresh();
+  }
 
-      switch (result) {
-        case Ok<List<Exam>>():
-          _exams = result.value;
-          break;
-        case Error<Exam>():
-          // TODO log warning / error
-          break;
-        default:
+  void _refresh() {
+    _updatePages();
+    sceneReady.value = true;
+
+    _pageController.addListener(() {
+      final page = _pageController.page ?? 0.0;
+      final intPart = page.floor();
+      final decimalPart = page - intPart;
+
+      if (decimalPart != 0 && intPart > 0 && intPart < exams.value.length) {
+        var lIndex = intPart - 1;
+        var rIndex = intPart;
+
+        exams.value.elementAt(lIndex).rotate(decimalPart * turnOffset.radians);
+        exams.value
+            .elementAt(rIndex)
+            .rotate(-(1 - decimalPart) * turnOffset.radians);
       }
-    } finally {
-      _initialize();
+      notifyListeners();
+    });
+
+    if (exams.value.isNotEmpty && _pageController.hasClients) {
+      _pageController.jumpToPage(1);
     }
   }
 
-  void _updatePageCount() {
-    pageCount.value = _exams.length + 1;
-    notifyListeners();
-  }
-
   void _updatePages() {
-    final examPages = _exams.asMap().entries.map((entry) {
+    final examPages = exams.value.asMap().entries.map((entry) {
       final index = entry.key;
       final exam = entry.value;
 
       final lVerticalText = switch (index) {
         0 => 'NEW EXAM',
-        _ => _exams.elementAt(index - 1).name,
+        _ => exams.value.elementAt(index - 1).name,
       };
 
       var rVerticalText = '';
-      if (index + 1 < _exams.length) {
-        rVerticalText = _exams.elementAt(index + 1).name;
+      if (index + 1 < exams.value.length) {
+        rVerticalText = exams.value.elementAt(index + 1).name;
       }
 
-      return ExamPageWidget(
-        rotationViewModel: rotationViewModels[index],
+      return ExamPage(
         exam: exam,
         lVerticalText: lVerticalText,
         rVerticalText: rVerticalText,
       );
     });
-    _pages = [
-      AddExamPage(rVerticalText: _exams.firstOrNull?.name ?? ''),
-      ...examPages,
-    ];
+    pages.value = [...examPages];
   }
 
-  void _initialize() async {
-    rotationViewModels = _exams.map((_) => RotationViewModel()).toList();
-
-    _updatePages();
-    _updatePageCount();
-
-    sceneReady.value = true;
-
-    notifyListeners();
+  Future<void> _addExam(Exam exam) async {
+    await _examRepository.create(exam);
+    await _load();
   }
 
-  void _init(PageController pageController) {
-    pageController.addListener(() {
-      final page = pageController.page ?? 0.0;
-      final intPart = page.floor();
-      final decimalPart = page - intPart;
-
-      if (decimalPart != 0 && intPart > 0 && intPart < _exams.length) {
-        var lIndex = intPart - 1;
-        var rIndex = intPart;
-
-        rotationViewModels
-            .elementAt(lIndex)
-            .rotateCommand
-            .execute(decimalPart * turnOffset.radians);
-
-        rotationViewModels
-            .elementAt(rIndex)
-            .rotateCommand
-            .execute(-(1 - decimalPart) * turnOffset.radians);
-      }
-    });
+  void addExamsListener(VoidCallback listener) {
+    exams.addListener(listener);
   }
 
-  @override
-  void dispose() {
-    for (var vm in rotationViewModels) {
-      vm.dispose();
-    }
-    super.dispose();
+  void removeExamsListener(VoidCallback listener) {
+    exams.removeListener(listener);
   }
 }
 
