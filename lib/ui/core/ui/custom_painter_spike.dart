158cf411bc68d1281f4bbc263f1f37111402c45c
Marco <marco.baresi01@gmail.com>
2025-08-06 21:58:56 +0200

    [spike] curved line painter (#59)

--- lib/ui/core/ui/custom_painter_spike.dart
+++ lib/ui/core/ui/custom_painter_spike.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class LinePainter extends CustomPainter {
@@ -47,6 +49,57 @@ class CirclePainter extends CustomPainter {
     final center = Offset(size.shortestSide / 2, size.shortestSide / 2);
     final radius = size.shortestSide / 4;
     canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class CurvedLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint =
+        Paint()
+          ..color = Colors.white54
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 24.0
+          ..strokeCap = StrokeCap.round
+          ..strokeJoin = StrokeJoin.round;
+
+    final path = Path();
+    path.moveTo(size.width * 0.5, 0);
+
+    final paint2 =
+        Paint()
+          ..color = Colors.white
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 24.0
+          ..strokeCap = StrokeCap.round
+          ..strokeJoin = StrokeJoin.round;
+
+    final path2 = Path();
+    path2.moveTo(size.width * 0.5, 0);
+    // path.quadraticBezierTo(
+    //   0,
+    //   size.height * 0.25,
+    //   size.width * 0.5,
+    //   size.height * 0.5,
+    // );
+    // path.quadraticBezierTo(
+    //   size.width,
+    //   size.height * 0.75,
+    //   size.width * 0.5,
+    //   size.height,
+    // );
+
+    // path.quadraticBezierTo(
+    //   -size.height * 0.25,
+    //   size.height * 0.25,
+    //   size.width * 0.5,
+    //   size.height * 0.5,
+    // );
+
+    canvas.drawPath(path, paint);
   }
 
   @override
