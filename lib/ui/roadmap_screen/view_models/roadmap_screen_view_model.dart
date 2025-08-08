ce739fdcf52863343cdc38e689ff9e47bf9baafe
Marco <marco.baresi01@gmail.com>
2025-08-08 22:23:10 +0200

    [wip] create roadmap screen view model (#59)

---
+++ lib/ui/roadmap_screen/view_models/roadmap_screen_view_model.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_command/flutter_command.dart';
+import 'package:unizen/utils/result.dart';
+
+import '../../../data/repositories/exam/exam_repository.dart';
+import '../../../domain/models/exam/exam.dart';
+
+enum RoadmapScreenState { initial, loading, loaded, error }
+
+class RoadmapScreenViewModel extends ChangeNotifier {
+  RoadmapScreenViewModel({required ExamRepository examRepository})
+    : _examRepository = examRepository {
+    state = ValueNotifier(RoadmapScreenState.initial);
+
+    exams = ValueNotifier<List<Exam>>([]);
+
+    loadCommand = Command.createAsyncNoParamNoResult(_load);
+    addExamCommand = Command.createAsyncNoResult<Exam>(_addExam);
+  }
+
+  final ExamRepository _examRepository;
+
+  late final ValueNotifier<List<Exam>> exams;
+  late final ValueNotifier<RoadmapScreenState> state;
+
+  late final Command<void, void> loadCommand;
+  late final Command<Exam, void> addExamCommand;
+
+  Future<void> _load() async {
+    state.value = RoadmapScreenState.loading;
+
+    final result = await _examRepository.getExamsList();
+    switch (result) {
+      case Ok<List<Exam>>():
+        exams.value = result.value;
+        state.value = RoadmapScreenState.loaded;
+        break;
+      case Error<List<Exam>>():
+        state.value = RoadmapScreenState.error;
+        exams.value = [];
+        break;
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> _addExam(Exam exam) async {
+    await _examRepository.create(exam);
+    await _load();
+  }
+}
