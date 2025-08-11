328f8321620fd27a487b91c3692fc4f519cd52db
Marco <marco.baresi01@gmail.com>
2025-08-11 23:32:19 +0200

    update local data service bosses distances to better occupy available space (#61)

--- lib/data/services/local/local_data_service.dart
+++ lib/data/services/local/local_data_service.dart
@@ -5,14 +5,14 @@ import '../../../domain/models/exam/exam.dart';
 class LocalDataService {
   List<Exam> getExams() {
     return [
+      // Exam(
+      //   name: 'MACHINE LEARNING',
+      //   maxHealth: 5000,
+      //   health: 2780,
+      //   boss: getBosses().elementAt(4),
+      // ),
       Exam(
-        name: 'MACHINE LEARNING',
-        maxHealth: 5000,
-        health: 2780,
-        boss: getBosses().elementAt(4),
-      ),
-      Exam(
-        name: 'AUTOMATION',
+        name: 'AUTOMATION AND TECHNOLOGY',
         maxHealth: 5000,
         health: 2780,
         boss: getBosses().elementAt(5),
@@ -23,30 +23,30 @@ class LocalDataService {
         health: 4878,
         boss: getBosses().elementAt(1),
       ),
-      Exam(
-        name: 'COMPUTER SCIENCE',
-        maxHealth: 5000,
-        health: 1280,
-        boss: getBosses().elementAt(0),
-      ),
-      Exam(
-        name: 'ARTIFICIAL INTELLIGENCE',
-        maxHealth: 5000,
-        health: 2780,
-        boss: getBosses().elementAt(6),
-      ),
-      Exam(
-        name: 'AUTOMATION',
-        maxHealth: 5000,
-        health: 2780,
-        boss: getBosses().elementAt(3),
-      ),
-      Exam(
-        name: 'AI ENTREPRENEURSHIP',
-        maxHealth: 5000,
-        health: 2780,
-        boss: getBosses().elementAt(2),
-      ),
+      // Exam(
+      //   name: 'COMPUTER SCIENCE',
+      //   maxHealth: 5000,
+      //   health: 1280,
+      //   boss: getBosses().elementAt(0),
+      // ),
+      // Exam(
+      //   name: 'ARTIFICIAL INTELLIGENCE',
+      //   maxHealth: 5000,
+      //   health: 2780,
+      //   boss: getBosses().elementAt(6),
+      // ),
+      // Exam(
+      //   name: 'AUTOMATION',
+      //   maxHealth: 5000,
+      //   health: 2780,
+      //   boss: getBosses().elementAt(3),
+      // ),
+      // Exam(
+      //   name: 'AI ENTREPRENEURSHIP',
+      //   maxHealth: 5000,
+      //   health: 2780,
+      //   boss: getBosses().elementAt(2),
+      // ),
     ];
   }
 
@@ -55,21 +55,21 @@ class LocalDataService {
       Boss(
         animatedScene: AnimatedScene(
           modelAssetPath: 'build/models/tvwoman.model',
-          cameraDistance: 28,
+          cameraDistance: 22,
         ),
         ects: 3,
       ),
       Boss(
         animatedScene: AnimatedScene(
           modelAssetPath: 'build/models/cameraman_supreme_god.model',
-          cameraDistance: 36,
+          cameraDistance: 28,
         ),
         ects: 4,
       ),
       Boss(
         animatedScene: AnimatedScene(
           modelAssetPath: 'build/models/skibidi_yisus.model',
-          cameraDistance: 28,
+          cameraDistance: 22,
         ),
         ects: 5,
       ),
@@ -90,7 +90,7 @@ class LocalDataService {
       Boss(
         animatedScene: AnimatedScene(
           modelAssetPath: 'build/models/tvman_multiple.model',
-          cameraDistance: 32,
+          cameraDistance: 22,
         ),
         ects: 8,
       ),
