ee588a76b1f68e90fd834c32d2a391012fe830fe
Marco <marco.baresi01@gmail.com>
2025-08-19 22:40:33 +0200

    [wip] update add exam page widget with some placeholders (#61)

--- lib/ui/home_screen/widgets/add_exam_page_widget.dart
+++ lib/ui/home_screen/widgets/add_exam_page_widget.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:unizen/ui/animated_scene/animated_scene.dart';
 import 'package:unizen/ui/core/ui/custom_painter_spike.dart';
 
+import '../../../domain/models/animated_scene/animated_scene.dart';
+import '../../../domain/models/boss/boss.dart';
+import '../../../domain/models/exam/exam.dart';
+import '../../../domain/models/health_bar/health_bar.dart';
 import '../../../ui/core/ui/liquid_glass_icon_button.dart';
+import '../../core/ui/overlay_text.dart';
+import '../../health_bar/view_models/health_bar_view_model.dart';
+import '../../health_bar/widgets/health_bar_widget.dart';
 import '../view_models/add_exam_page_view_model.dart';
 import '../view_models/home_screen_view_model.dart';
-import 'add_exam_modal.dart';
 
 class AddExamPageWidget extends StatelessWidget {
   const AddExamPageWidget({
@@ -18,31 +25,102 @@ class AddExamPageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: SizedBox(
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: 500,
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: CustomPaint(painter: LinePainter()),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                _AnimatedSceneSection(
+                  height: 150,
+                  exam: Exam(
+                    name: 'AUTOMATION',
+                    maxHealth: 5000,
+                    health: 2780,
+                    boss: Boss(
+                      animatedScene: AnimatedScene(
+                        modelAssetPath: 'build/models/tv_man_supreme.model',
+                        cameraDistance: 16,
+                      ),
+                      ects: 6,
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 200,
-                child: CustomPaint(painter: RectanglePainter()),
-              ),
-              LiquidGlassIconButton(
-                icon: Icons.add_rounded,
-                size: 80.0,
-                onPressed: () => _onAddExamPressed(context),
-              ),
-            ],
+                const SizedBox(height: 8.0),
+                _HealthBarSection(
+                  exam: Exam(
+                    name: 'AUTOMATION',
+                    maxHealth: 5000,
+                    health: 2780,
+                    boss: Boss(
+                      animatedScene: AnimatedScene(
+                        modelAssetPath: 'build/models/tv_man_supreme.model',
+                        cameraDistance: 16,
+                      ),
+                      ects: 6,
+                    ),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 48.0),
+                ),
+                SizedBox(height: 310),
+                Align(
+                  alignment: AlignmentGeometry.centerRight,
+                  child: LiquidGlassIconButton(
+                    icon: Icons.add_rounded,
+                    size: 60.0,
+                    onPressed: () => _onAddExamPressed(context),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: CustomPaint(
+              painter: CurvedZigZagPainter(curveCount: 3, curveHeight: 160),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                SizedBox(height: 156),
+                _AnimatedSceneSection(
+                  height: 150,
+                  exam: Exam(
+                    name: 'MACHINE LEARNING',
+                    maxHealth: 5000,
+                    health: 2780,
+                    boss: Boss(
+                      animatedScene: AnimatedScene(
+                        modelAssetPath: 'build/models/tvman_supreme.model',
+                        cameraDistance: 44,
+                      ),
+                      ects: 3,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                _HealthBarSection(
+                  exam: Exam(
+                    name: 'MACHINE LEARNING',
+                    maxHealth: 4800,
+                    health: 4027,
+                    boss: Boss(
+                      animatedScene: AnimatedScene(
+                        modelAssetPath: 'build/models/tvman_supreme.model',
+                        cameraDistance: 44,
+                      ),
+                      ects: 3,
+                    ),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 48.0),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -56,12 +134,56 @@ class AddExamPageWidget extends StatelessWidget {
       builder: (BuildContext context) {
         return SizedBox(
           height: MediaQuery.of(context).size.height - 120.0,
-          child: AddExamModal(
-            viewModel: viewModel,
-            homeScreenViewModel: homeScreenViewModel,
-          ),
+          // child: AddExamModal(
+          //   viewModel: viewModel,
+          //   timelineScreenViewModel: homeScreenViewModel,
+          // ),
         );
       },
+    );
+  }
+}
+
+class _AnimatedSceneSection extends StatelessWidget {
+  const _AnimatedSceneSection({required this.exam, required this.height});
+
+  final Exam exam;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        OverlayText(
+          exam.name,
+          margin: const EdgeInsets.symmetric(horizontal: 48.0),
+        ),
+        SizedBox(
+          height: height,
+          width: double.infinity,
+          child: AnimatedSceneWidget(exam: exam),
+        ),
+      ],
+    );
+  }
+}
+
+class _HealthBarSection extends StatelessWidget {
+  const _HealthBarSection({required this.exam, this.margin});
+
+  final Exam exam;
+  final EdgeInsets? margin;
+
+  @override
+  Widget build(BuildContext context) {
+    return HealthBarWidget(
+      viewModel: HealthBarViewModel(
+        config: HealthBar(size: HealthBarSize.medium),
+        maxHealth: exam.maxHealth,
+        health: exam.health,
+      ),
+      margin: margin,
     );
   }
 }
